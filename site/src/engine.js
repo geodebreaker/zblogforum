3a7fcdfda3537e9de6df47c75c600d0ce0a59cba
geodebreaker <65726766+geodebreaker@users.noreply.github.com>
2024-10-28 21:38:34 +0000

    modified:   site/index.html 	modified:   site/src/engine.js 	modified:   svr/node.js

--- site/src/engine.js
+++ site/src/engine.js
@@ -1,19 +1,29 @@
 $ = x => document.querySelector(x);
 
-async function go(loc) {
-  if (loc)
+function init() {
+  window.onpopstate = (e) => new Promise(y => y(go(null, e.state)));
+  go()
+}
+
+async function go(loc, stat) {
+  if (loc && !stat)
     history.pushState({}, '', loc);
-  var c = await fetch('/api/content?q=' + encodeURI(loc ?? location.pathname)).then(
-    r => r.ok ? r.json() : { fail: ['Server provided response: ', r.text()] },
-    e => ({ fail: 'Error occured: ' + e }));
-  if (c.fail)
-    return err('Failed to fetch page content. Please reload.',
-      (c.fail instanceof Array ? c.fail[0] + await c.fail[1] : c.fail));
-  $('#content').innerHTML = c.page;
+  if (!(stat && stat.ttl >= Date.now() - 60e3)) {
+    var c = await fetch('/api/content?q=' + encodeURI(loc ?? location.pathname)).then(
+      r => r.ok ? r.json() : { fail: ['Server provided response: ', r.text()] },
+      e => ({ fail: 'Error occured: ' + e }));
+    if (c.fail)
+      return err('Failed to fetch page content. Please reload.',
+        (c.fail instanceof Array ? c.fail[0] + await c.fail[1] : c.fail));
+    history.replaceState({ttl: Date.now(), stat: c}, '', loc);
+  } else {
+    c = stat.stat;
+  }
+  $('#content').innerHTML = c.html;
 }
 
 function err(...e) {
   alert(e.join('\n'))
 }
 
-go();
+init();
--- svr/node.js
+++ svr/node.js
@@ -1,13 +1,28 @@
+const URL = require('url').URL;
 const getfile = require('fs').readFileSync;
 const extToMIME = {
   html: 'text/html',
-  js: 'application/js',
+  txt: 'text/plain',
+  css: 'text/css',
+  js: 'application/javascript',
+  json: 'application/json',
+  svg: 'image/svg+xml',
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  gif: 'image/gif',
+  webp: 'image/webp',
+  ico: 'image/x-icon',
+  mp3: 'audio/mpeg',
+  wav: 'audio/wav',
+  mp4: 'video/mp4',
+  webm: 'video/webm',
+  ttf: 'font/ttf',
+  zip: 'application/zip',
 };
 
 require('http').createServer((req, res) => {
-  var url = req.url;
-  url = url.replaceAll('//', '/');
-  url = url.split('/')[0];
+  var params = Object.fromEntries(new URL(req.url, 'http://a/').searchParams.entries());
+  var url = req.url.replace(/^\//, '').replaceAll('//', '/').replace(/\?.+$/i, '').split('/');
   var type = url.length == 1 ? 0 : url.shift();
   if (!type == 0) {
     if (type == 'src')
@@ -17,27 +32,64 @@ require('http').createServer((req, res) => {
     else
       type = 0;
   }
+  url = url.join('/');
   if (type == 0) {
+    console.log('MAIN:', url);
     var file = getfile('./site/index.html');
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(file);
+  } else if (type == 1) {
+    src(res, url)
+  } else if (type == 2) {
+    api(res, url, params)
+  } else {
+    console.log('NCF 500:', url);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('500 Internal Server Error\n\n-- ZBF --');
   }
 }).listen(8080);
 
-function src(url) {
-
+function src(res, url) {
   try {
-    console.log('200:', type, url);
-    var file = getfile('./site' + url);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var file = getfile('./site/src/' + url);
+    console.log('SRC 200:', url);
+    res.writeHead(200, { 'Content-Type': extToMIME[url.match(/(?<=\.)[a-z]{2,4}$/i)] ?? 'text/html' });
     res.end(file);
   } catch (e) {
-    console.log('404:', type, url);
+    console.log('SRC 404:', url);
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found\n\n-- ZBF --');
   }
 }
 
-function api(url, params) {
+function api(res, url, params) {
+  const ret = x => {
+    var status = x ? 200 : 500;
+    console.log('API ' + status + ':', url);
+    res.writeHead(status, { 'Content-Type': x ? 'application/json' : 'text/plain' });
+    res.end(x ? JSON.stringify(x) : '500 Internal Server Error\n\n-- ZBF --');
+  };
+  switch (url) {
+    case 'content':
+      if(!params.q)
+        return ret();
+      ret(content(params.q));
+      break;
+    default:
+      ret();
+      break;
+  }
+}
 
+function content(ourl) {
+  var url = ourl.split('/');
+  url.shift();
+  var type = url.shift() ?? '';
+  url = url.join('/');
+  switch (type) {
+    case '':
+      return {type: 'html', html: `<h3>MAIN PAGE</h3><hr><div>TIME: ${Date.now().toString(16).slice(-8, -2)}</div>`};
+    default:
+      return {type: 'html', html: '404 Not Found'};
+  }
 }
